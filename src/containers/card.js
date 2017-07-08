738fd9b7a08426fb9f4f5de8c79d8a6b4511bb3f
karlpet <karlpetter.svensson@hotmail.se>
2017-07-08 18:59:14 +0200

    BEM naming scheme added

--- src/containers/card.js
+++ src/containers/card.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Card = ({ title, instructions, progress, children }) => (
     <div className="card">
-        <h1>{title}</h1>
-        <img src={progress} alt="Progress" />
-        <hr/>
-        <h2>{instructions}</h2>
+        <h1 className="card__title">{title}</h1>
+        <img className="card__progress" src={progress} alt="Progress" />
+        <hr className="card__divider"/>
+        <h2 className="card__instructions">{instructions}</h2>
         <div className="card__content">{children}</div>
     </div>
 )
