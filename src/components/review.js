45cc4e2dc603665f8a11850958cbba2cbd98a32b
Carl Petter Svensson <karlpetter.svensson91@gmail.com>
2017-07-13 02:02:10 +0200

    Review section finished

--- src/components/review.js
+++ src/components/review.js
@@ -6,21 +6,40 @@ import { Card } from '../containers/card'
 
 const ReviewDummy = ({ backToScheduleTime, send, questions, users, schedule }) => {
     
-    let usersDisplay = users.reduce((usersString, user) => usersString + user.name + ", ", "")
+    let usersDisplay = users.filter(user=>user.selected).reduce((usersString, user) => usersString + user.name + ", ", "")
     usersDisplay = usersDisplay.substring(0, usersDisplay.length - 2)
+
+    let weekday = new Array(7);
+    weekday[0] = "sunday";
+    weekday[1] = "monday";
+    weekday[2] = "tuesday";
+    weekday[3] = "wednesday";
+    weekday[4] = "thursday";
+    weekday[5] = "friday";
+    weekday[6] = "saturday";
 
     return (
     <Card title="Great! You're already done!" 
           instructions="Review:" 
           progress={require("../images/progress_4.png")}>
-        <h1>Questions({questions.length}):</h1>
+        <h1 className="review__title">Questions({questions.length}):</h1>
         {questions.map((question, index) => 
-          <p key={index}>{question}</p>
+          <p className="review__content" key={index}>{question}</p>
         )}
-        <h1>Users({users.length}):</h1>
-        <p>{usersDisplay}</p>
+        <h1 className="review__title">Users({users.filter(user => user.selected).length}):</h1>
+        <p className="review__content">{usersDisplay}</p>
 
-        <p>Your check-ins will be sent out</p>
+        <p className="review__content review__summary">Your check-ins will be sent out</p>
+        <p className="review__content">
+            every {schedule.repeats.substring(0,schedule.repeats.length-2)} at 
+            {' ' + weekday.filter(day => schedule.repeat_on[day.substring(0,3)]).join(', ')}
+            {' ' + schedule.time} 
+             {(() => {
+              const split = new Date().toString().split(" ");
+              const timeZoneFormatted = split[split.length - 2] + " " + split[split.length - 1];
+              return ' ' + timeZoneFormatted
+            })()}.
+        </p>
         <a className="button button-next button-shared" onClick={send}>Finish</a>
         <a className="button button-back button-shared-back" onClick={backToScheduleTime}>Back</a>
     </Card>
