98f0501cf36a30c4b0a5d0a712ada1ae3f72da3a
Carl Petter Svensson <karlpetter.svensson91@gmail.com>
2017-07-30 22:04:43 +0200

    user object cleanup.

--- src/reducers/onboarding.js
+++ src/reducers/onboarding.js
@@ -46,14 +46,14 @@ export default function onBoarding(state = initialState, action) {
           console.log(action.rawUsers)
           const users = action.rawUsers
             .filter(user => !user.is_bot)
-            .map(user => Object.assign({},user,{ selected: true }))
-          return Object.assign({}, state, { users: users })
+            .map(user => Object.assign(user,{ selected: true }))
+          return Object.assign(state, { users: users })
 
         case SWITCH_CARD:
-          return Object.assign({}, state, { currentCard: action.card})
+          return Object.assign(state, { currentCard: action.card})
 
         case SAVE_QUESTIONS:
-          return Object.assign({}, state, { questions: action.questions})
+          return Object.assign(state, { questions: action.questions})
 
         case SEND:
           console.log("sending data!")
@@ -63,45 +63,45 @@ export default function onBoarding(state = initialState, action) {
           const newUsers = [].concat(state.users)
           newUsers[action.index].selected = !newUsers[action.index].selected
           const allSelected = newUsers.every((user) => user.selected)
-          return Object.assign({}, state,{ users: newUsers},{ allSelected: allSelected })
+          return Object.assign(state,{ users: newUsers},{ allSelected: allSelected })
 
         case TOGGLE_ALL_USERS:
           const newAllSelected = !state.allSelected
-          const toggleAllUsers = state.users.map(user => Object.assign({}, user, { selected: newAllSelected }))
-          return Object.assign({}, state, {users: toggleAllUsers},{ allSelected: newAllSelected })
+          const toggleAllUsers = state.users.map(user => Object.assign(user, { selected: newAllSelected }))
+          return Object.assign(state, {users: toggleAllUsers},{ allSelected: newAllSelected })
 
         case SELECT_USER:
           const newSelectUsers = [].concat(state.users)
           newSelectUsers[action.index].selected = true
           const allSelectedSelect = newSelectUsers.every((user) => user.selected)
-          return Object.assign({}, state,{ users: newSelectUsers},{ allSelected: allSelectedSelect })
+          return Object.assign(state,{ users: newSelectUsers},{ allSelected: allSelectedSelect })
 
         case TOGGLE_DAY_REPEAT:
           const newDayRepeatSchedule = Object.assign(state.schedule)
           newDayRepeatSchedule.repeat_on[action.day] = !newDayRepeatSchedule.repeat_on[action.day]
-          return Object.assign({}, state, { schedule: newDayRepeatSchedule })
+          return Object.assign(state, { schedule: newDayRepeatSchedule })
 
         case SELECT_REPEAT_EVERY:
           const newRepeatEverySchedule = Object.assign(state.schedule)
           newRepeatEverySchedule.repeat_every = action.value
-          return Object.assign({}, state, { schedule: newRepeatEverySchedule })
+          return Object.assign(state, { schedule: newRepeatEverySchedule })
 
         case SELECT_REPEATS:
-          const newSelectRepeatsSchedule = Object.assign({},state.schedule)
+          const newSelectRepeatsSchedule = Object.assign(state.schedule)
           newSelectRepeatsSchedule.repeats = action.interval
-          return Object.assign( {},state,{ schedule: newSelectRepeatsSchedule})
+          return Object.assign(state,{ schedule: newSelectRepeatsSchedule})
 
         case SELECT_TIME:
           const newSelectTimeSchedule = Object.assign( state.schedule )
           newSelectTimeSchedule.time = action.time
-          return Object.assign({ state, schedule: newSelectTimeSchedule })
+          return Object.assign(state, {schedule: newSelectTimeSchedule})
         
         case SAVE_CODE:
-          return Object.assign({},state, {token: action.code })
+          return Object.assign(state, {token: action.code })
         
         case SAVE_TOKEN:
           console.log(action.token)
-          return Object.assign({ state, token: action.token })
+          return Object.assign(state,{ token: action.token })
 
         default:
           return state
