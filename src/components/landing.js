0a374327007620eb10ff3393c4c5097fa8b03d3a
Carl Petter Svensson <karlpetter.svensson91@gmail.com>
2017-09-20 19:52:57 +0200

    landing page is now looking as intended.

--- src/components/landing.js
+++ src/components/landing.js
@@ -8,7 +8,7 @@ export const Landing = () => (
         <div className="landing__signin">
             <section className="landing__hero">
                 <h1>Keep your team <em>happy and engaged</em></h1>
-                <p>Automate <em>check-ins</em> with<br/>your team in Slack.</p>
+                <p>Automate <em>check-ins</em> with your team in Slack.</p>
                 <a href="https://www.slack.com/oauth/authorize?scope=users.profile:read,users:read,chat:write:user,channels:read,im:read,team:read,im:history&client_id=143457452320.144253511221">
                     <img alt="Add to Slack" 
                          height="80"
@@ -19,10 +19,22 @@ export const Landing = () => (
         </div>
         <div className="landing__how">
             <h1>Automatically send <em>scheduled messages</em> to your team in <em>Slack</em>!</h1>
-            <div className="how__slackimage">SLACKIMAGE</div>
+            <div>
+                <img className="how__slackimage" src={require('../images/iphone_template.png')} />
+                <div>
+                    <p className="how__left">Use our suggested messages, or create your own. Select which team members to send them to.</p>
+                    <p className="get__started">Get started in <em>60 seconds</em>!</p>
+                    <img className="how__onboardimage" src={require('../images/desktop_landing_example.png')} />
+                </div>
+            </div>
+        </div>
+
+        <div className="landing__how__mobile">
+            <h1>Automatically send <em>scheduled messages</em> to your team in <em>Slack</em>!</h1>
+            <img className="how__slackimage" src={require('../images/mobile_template.png')} />
             <p>Use our suggested messages, or create your own. Select which team members to send them to.</p>
             <p>Get started in <em>60 seconds</em>!</p>
-            <div className="how__onboardimage">ONBOARDIMAGE</div>
+            <img className="how__onboardimage" src={require('../images/mobile_landing_example.png')} />
         </div>
         <div className="landing__signinbottom">
             <h1>Get started with your<br/>manager one on ones</h1>
